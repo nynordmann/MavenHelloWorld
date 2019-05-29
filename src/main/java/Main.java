7562bb8188c642750e5c99558897d835638aa936
nynordmann <thorsten_czech@yahoo.de>
2019-05-29 08:20:31 -0700

    bla commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,7 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Maven Hello World!");
+        System.out.println("Maven to Hello World!");
     }
 
 }

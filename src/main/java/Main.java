bade208beabdbcc784e02cb16bc42dce8b8629e0
nynordmann <thorsten_czech@yahoo.de>
2019-05-29 08:39:48 -0700

    nochwas

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,7 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Maven to Hello World!");
+        System.out.println("Maven to new Hello World!");
     }
 
 }
